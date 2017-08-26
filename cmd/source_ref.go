c5a6c6dbfbfd2857504ac4ced829202d82ae4f63
Stephen Baker <stephen.baker@ticketmaster.com>
2017-08-25 18:50:29 -0700

    Comments

--- cmd/source_ref.go
+++ cmd/source_ref.go
@@ -6,11 +6,17 @@ import (
 	"github.com/tektsu/gedcom"
 )
 
+// sourceRef describes a source reference.
+// RefNum is the numeric id of the source from the Gedcom file.
+// Ref is a string representation of the source, and may contain additional
+// information when a sourceRef is built from a citation.
 type sourceRef struct {
 	RefNum int
 	Ref    string
 }
 
+// newSourceRef build a sourceRef from a gedcom.SourceRecord.
+// A poorly-formed Gedcam file can produce a panic.
 func newSourceRef(s *gedcom.SourceRecord) *sourceRef {
 	refNum, err := strconv.Atoi(s.Xref[1:len(s.Xref)])
 	if err != nil {
@@ -23,6 +29,8 @@ func newSourceRef(s *gedcom.SourceRecord) *sourceRef {
 	}
 }
 
+// newSourceRefFromCitation build a sourceRef from a gedcom.CitationRecord.
+// A poorly-formed Gedcam file can produce a panic.
 func newSourceRefFromCitation(c *gedcom.CitationRecord) *sourceRef {
 	r := newSourceRef(c.Source)
 	return r
